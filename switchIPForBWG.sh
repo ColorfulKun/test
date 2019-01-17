68bc4582fef4615df3964cc4d14da2040b94e142
Colorful <827423346@qq.com>
2019-01-17 19:08:47 +0800

    123

--- switchIPForBWG.sh
+++ switchIPForBWG.sh
@@ -5,7 +5,7 @@
 # Created Time: Thu 17 Jan 2019 01:57:32 PM CST
 #########################################################################
 #!/bin/bash
-
+export LANG=en_US.UTF-8
 
 veid=1205610
 apiKey=private_cIPULsHJJNDZpiewIWo9FWO4
@@ -31,6 +31,8 @@ function send_email()
 	echo $1 | mail -s "搬瓦工IP切换 $DATE" 827423346@qq.com
 }
 
+
+log_normal "########## start switch BWG IP ##########"
 serviceInfo=$(curl -s "https://api.64clouds.com/v1/getServiceInfo?&veid=$veid&api_key=$apiKey")
 echo $serviceInfo | grep '"error":0' >/dev/null || {
 	log_fail "get service infomation failed"
@@ -40,7 +42,7 @@ echo $serviceInfo | grep '"error":0' >/dev/null || {
 	log_sucess "get service infomation success"
 }
 
-serviceIP=$(echo $serviceInfo | cut -d":" -f4 | cut -d"," -f1 |sed 's#"##g') && {
+serviceIP=$(echo $serviceInfo | cut -d":" -f22 | cut -d"\"" -f2 ) &&  {
 	log_normal "serviceIP = $serviceIP" 
 } || {
 	send_email "服务器原IP获取失败"
@@ -79,16 +81,15 @@ function switchLocal()
 	echo $result | grep "Please try again in 10-15 minutes." >/dev/null && {
 		log_fail "Migration backend is currently not available for this VPS. Please try again in 10-15 minutes."
 		send_email "搬瓦工服务器依然处于锁定状态，IP地址切换不成功"
-		exit 1
+		return 1
 	}
 	echo $result | grep "Unable to migrate into same location" >/dev/null && {
 		log_fail "Unable to migrate into same location,switchLocal again"
 		switchLocal
 	}
 }
-
 switchLocal
-
+log_normal "########## end switch BWG IP ##########"
 
 
 
